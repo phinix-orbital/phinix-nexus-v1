852320c9260cd97c63818de3847ab5d618fa459c
sandhanphinix <sandhan.borgohain@phinix.fr>
2024-05-10 23:17:39 +0100

    successful imp of generate_template

--- src/exe/generate_template.py
+++ src/exe/generate_template.py
@@ -17,7 +17,7 @@ class GenerateTemplate:
     def parse_args(cls) -> Namespace:
         parser = ArgumentParser(description="Script to generate templates from Jinja files.")
         parser.add_argument("template_name", help="Jinja template name to use for file generation.")
-        parser.add_argument("-n", "--number_of_files", type=int, help="Number of files to generate from the template.")
+        parser.add_argument("-n", "--number_of_files", type=int, default=1, help="Number of files to generate from the template.")
         parser.add_argument("-e", "--extension", help="File extension of files generated from the template.")
         parser.add_argument("-l", "--list_filenames", type=list, help="Optional argument providing list of filenames to use.")
         parser.add_argument("-d", "--directory", default="stock/configs/components", help="Default directory within src to store generated templates.")
@@ -26,7 +26,7 @@ class GenerateTemplate:
     
     @classmethod
     def main(cls):
-        _inputs: Namespace = cls.parse_args()        
+        _inputs: Namespace = cls.parse_args()     
         _temp_name = _inputs.template_name
         if _temp_name[-3:]!=".j2":
             _temp_name = os.path.splitext(_temp_name)[0] + ".j2"
@@ -55,19 +55,22 @@ class GenerateTemplate:
         except ValidationError as e:
             raise e
         _dir = os.path.join(GenericHelpers.get_base_path(), "src", _inputs.directory)
-        try:
-            _ = ValidateLocalFilePath(file_path=_dir)
-        except ValidationError as e:
-            raise e
+        if not os.path.exists(_dir):
+            raise ValueError(f"{_dir} directory does not exist!")
         if not _fnames:
             _fnames = []
             for i in range(n_files):
-                _fnames.append(GenericHelpers.generate_random_name(output_dir=_dir))
+                _fnames.append(GenericHelpers.generate_random_name(output_dir=_dir, seed=i))
         if _ext:
             _fnames = [f"{i}.{_ext}" for i in [os.path.splitext(j)[0] for j in _fnames]]
         _template_env = Environment(loader=FileSystemLoader(searchpath=_temp_dir))
         _template = _template_env.get_template(_temp_name)
+        logger.info(f"--- Generating templates at dir {_dir}... ---")
         for f in _fnames:
-            _file = open(os.path.join(_dir, f), "wb")
+            _file = open(os.path.join(_dir, f), "w")
             _file.write(_template.render())
             _file.close()
+        logger.info(f"--- Template generation completed successfully! ---")
+
+if __name__ == "__main__":
+    GenerateTemplate.main()
